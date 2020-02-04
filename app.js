213022df2682cb6a2217e5fb8e1a6a8f58d0dbec
Vuthy <suonx002@gmail.com>
2020-02-04 12:30:14 -0600

    temporary menu

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+const navList = document.querySelector('.nav__list');
+const burgerMenu = document.querySelector('.nav__burger');
+
 AOS.init();
 const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000
+});
+
+burgerMenu.addEventListener('click', e => {
+  navList.classList.toggle('show');
+});
+
+navList.addEventListener('click', () => {
+  navList.classList.toggle('show');
 });
